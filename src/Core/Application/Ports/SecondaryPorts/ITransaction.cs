99ac95c249638db866bd3fb8269f9d2f988e364a
Yandolav <yandolav@mail.ru>
2025-11-24 21:05:45 +0300

    fix 2

--- src/Core/Application/Ports/SecondaryPorts/ITransaction.cs
+++
@@ -1,6 +0,0 @@
-namespace Core.Application.Ports.SecondaryPorts;
-
-public interface ITransaction : IAsyncDisposable
-{
-    Task CommitAsync(CancellationToken cancellationToken);
-}
--- src/Core/Application/Ports/SecondaryPorts/ITransactionManager.cs
+++
@@ -1,6 +0,0 @@
-namespace Core.Application.Ports.SecondaryPorts;
-
-public interface ITransactionManager
-{
-    Task<ITransaction> BeginAsync(CancellationToken cancellationToken);
-}
